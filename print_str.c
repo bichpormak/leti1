65a9590e418930114f9befc9c2e377d9fe716326
bichpormak <lev_pankratov@mail.ru>
2023-10-19 07:47:23 +0300

    ee

---
+++ print_str.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "get_name.h"
+
+void print_str() {
+	printf("%s", get_name());
+
+}
---
+++ print_str.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_STR_H
+#define PRINT_STR_H
+
+void print_str();
+
+#endif
+
